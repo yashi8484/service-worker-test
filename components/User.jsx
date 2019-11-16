a049796f9d9cdcaa01aafa3411891db4c4bb87b2
yashi8484 <kobayashi@giftee.co>
2019-11-17 00:52:21 +0900

    ユーザーページの表示項目を追加

--- components/User.jsx
+++ components/User.jsx
@@ -3,8 +3,14 @@ import React from 'react';
 export const User = ({ model }) => {
   return (
     <>
+      <div>id: {model.id}</div>
       <div>name: {model.name}</div>
+      <div>username: {model.username}</div>
       <div>email: {model.email}</div>
+      <div>address: {model.address.street}</div>
+      <div>phone: {model.phone}</div>
+      <div>website: {model.website}</div>
+      <div>company: {model.company.name}</div>
     </>
   );
 };
